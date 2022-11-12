255fb5702528faa65df1dd5294f3e41432fd7d70
Julia Lima <julia.manvailer17@icloud.com>
2022-11-12 10:43:26 -0500

    fixed migrations

---
+++ migrations/20221112154143-updatedUser.js
@@ -0,0 +1,55 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      skills: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      passwordDigest: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      socialLinks: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      projects: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users')
+  }
+}
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const PostRouter = require('./PostRouter')
+const AuthRouter = require('./AuthRouter')
 
 Router.use('/users', UserRouter)
 Router.use('/posts', PostRouter)
+Router.use('/auth', AuthRouter)
 
 module.exports = Router
