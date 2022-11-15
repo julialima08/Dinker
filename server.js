760d75a84174e7d4904a88a9bc1df8144bac793a
Julia Lima <julia.manvailer17@icloud.com>
2022-11-15 12:14:01 -0500

    check session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const logger = require('morgan')
 
 const AppRouter = require('./routes/AppRouter')
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
+app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
